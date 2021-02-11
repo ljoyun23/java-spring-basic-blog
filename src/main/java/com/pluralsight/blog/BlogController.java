39cc1afe89696ce2ed2d4219862e90b570266d56
Lewina Jo <lewina.jo@anaplan.com>
2021-02-11 19:05:32 +0000

    Task 1,2,3

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,15 @@
 package com.pluralsight.blog;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BlogController {
 
+    @RequestMapping("/")
+    public String listPosts(ModelMap mmap) {
+        mmap.put("title", "Blog Post 1");
+        return "home";
+    }
 }
