336c1f0ea820cd852ea6a795c9a5769b5eefc31a
Lewina Jo <lewina.jo@anaplan.com>
2021-02-13 21:50:39 +0000

    First module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,15 @@
 package com.pluralsight.blog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BlogController {
 
     @RequestMapping("/")
-    public String listPosts(ModelMap mmap) {
-        mmap.put("title", "Blog Post 1");
+    public String listPosts(ModelMap modelMap) {
+        modelMap.put("title", "Blog Post 1");
         return "home";
     }
 }
